2468a65d1fde10066c72c8f60c27f4060bcdb56e
MartinImoberdorf <tinchoimo@gmail.com>
2025-08-20 23:40:36 -0300

    update

--- api-gateway/src/main/java/com/martin/microservices/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/martin/microservices/gateway/config/SecurityConfig.java
@@ -10,10 +10,23 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    // PAS0 27
+    private final String[] SWAGGER_WHITELIST = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/swagger-resources/**",
+            "/api-docs/**",
+            "/aggregate/**",
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(SWAGGER_WHITELIST).permitAll() // Permitir acceso a Swagger
+                        .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
     }
--- order-service/src/main/java/com/martin/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/martin/microservices/order/client/InventoryClient.java
@@ -1,19 +1,12 @@
 package com.martin.microservices.order.client;
-
-
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
 // PASO 21
 
-// le ponemos el nombre del cliente y la URL del servicio al que se va a conectar
-@FeignClient(value = "inventory", url = "${inventory.service.url}") // la URL la vamos a definir en el application.properties
+
 public interface InventoryClient {
     // definimos el método que queremos invocar en el servicio de inventario
-
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory") // este es el endpoint que vamos a invocar en el servicio de inventario
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
